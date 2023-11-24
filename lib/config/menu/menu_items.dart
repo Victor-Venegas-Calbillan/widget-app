d1ce35aaed9a6337d4f9a36ece7191e8d76a9014
Victor Venegas <victor.venegas.call@gmail.com>
2023-11-23 21:14:40 -0300

    preparando infinite scroll

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -57,4 +57,10 @@ const appMenuitems = <MenuItem>[
     link: '/tutorial',
     icon: Icons.accessibility_rounded,
   ),
+  MenuItem(
+    title: 'Infinite Scroll y Pull',
+    subTitle: 'Listas infinitas y pull to refresh',
+    link: '/infinite',
+    icon: Icons.list_alt_outlined,
+  ),
 ];
